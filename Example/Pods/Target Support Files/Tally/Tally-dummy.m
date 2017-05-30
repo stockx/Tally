eb84ed3b4055af6cf0f998fbed3e3dfa9a9d7738
Laurent Shala <laurentshala@icloud.com>
2017-05-30 12:10:21 -0400

    create simple demo app to show Tally in use

---
+++ Example/Pods/Target Support Files/Tally/Tally-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Tally : NSObject
+@end
+@implementation PodsDummy_Tally
+@end
--- Example/Tally/ViewController.swift
+++ Example/Tally/ViewController.swift
@@ -7,18 +7,22 @@
 //
 
 import UIKit
+import Tally
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let times = Tally.numberOfAppLaunches() == 1 ? "time" : "times"
+        label.text = "App did launch \(Tally.numberOfAppLaunches()) \(times)"
+        
+        if Tally.appHasLaunched(numberOfTimes: 3) {
+            let alertController = UIAlertController(title: "Threshold reached!", message: "Do anything you want!", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Dismis", style: .cancel, handler: nil))
+            present(alertController, animated: true)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
